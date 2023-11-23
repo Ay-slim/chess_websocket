e62e514b2167a5a4abd12fe283ac1bf072b2446e
Ay-slim <ayooluwaadedipe@gmail.com>
2023-11-23 17:36:14 +0000

    Extract move type into variable

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+type MoveType = {
+  srcSquareId: string;
+  targetSquareId: string;
+  pieceId: string;
+  opponentId: string;
+  promotionSquaresInfo?: {
+    src: string;
+    dest: string;
+  };
+};
+
 const httpServer = createServer();
 export const io = new Server(httpServer, {
   cors: {
@@ -16,21 +27,9 @@ httpServer.listen(port);
 console.log(`Web socket listening on port ${port}`);
 
 io.on("connection", (socket) => {
-  socket.on(
-    "validMove",
-    (move: {
-      srcSquareId: string;
-      targetSquareId: string;
-      pieceId: string;
-      opponentId: string;
-      promotionSquaresInfo?: {
-        src: string;
-        dest: string;
-      };
-    }) => {
-      io.emit(move.opponentId, move);
-    },
-  );
+  socket.on("validMove", (move: MoveType) => {
+    io.emit(move.opponentId, move);
+  });
 
   socket.on("joinedGame", (opponentId: string) => {
     io.emit(`${opponentId}-joined`);
